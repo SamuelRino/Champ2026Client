96ba6e37bf313e4fa9c8b2d38609a6b96b8eb8c4
SamuelRino <semenrunkin271@gmail.com>
2026-01-29 14:05:22 +0300

    v0.1.1

--- Champ2026Client/ModelsDTO/DisplayVmDTO.cs
+++ Champ2026Client/ModelsDTO/DisplayVmDTO.cs
@@ -18,5 +18,26 @@ namespace Champ2026Client.ModelsDTO
         public string OperatorName { get; set; } = null!;
         public int CommunicationQuality { get; set; }
         public DateTime LastRefreshTime { get; set; }
+        public int CurrentStock { get; set; }
+        public int MinStock { get; set; }
+        public int Bills { get; set; }
+        public string BillsCount { get; set; }
+        public int Coins { get; set; }
+        public string CoinsCount { get; set; }
+        public int Changes { get; set; }
+        public string ChangesCount { get; set; }
+        public string LastSale { get; set; }
+        public string LastEncashment { get; set; }
+        public string LastService { get; set; }
+        public int BillValidatorOk { get; set; }
+        public int CashlessOk { get; set; }
+        public int CashRegisterOk { get; set; }
+        public int DisplayOk { get; set; }
+        public int ControleMode { get; set; }
+        public int ComType { get; set; }
+        public int Audit { get; set; }
+        public int DifferentsSettings { get; set; }
+        public int EndedTimeWork { get; set; }
+        public int FarmOnline { get; set; }
     }
 }
